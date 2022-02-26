53cc6ed48ea105291b1eeaf8e8c179e2fb73a41a
victormmorales <victor.morales@criptan.com>
2022-02-26 13:49:16 +0100

    navbar styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import BasicLayout from "../layouts/BasicLayout/BasicLayout";
+
 export default function Home() {
   return (
-    <div className='home'>
-      <h1>Home</h1>
+    <div className="home">
+      <BasicLayout>
+        <h1>Home</h1>
+      </BasicLayout>
     </div>
-  )
+  );
 }
