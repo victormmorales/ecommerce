6c17ad617183372976867222ff56a97eec5e2093
victormmorales <victor.morales@criptan.com>
2022-02-28 09:17:07 +0100

    modal

--- components/Header/Menu/Menu.js
+++ components/Header/Menu/Menu.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import { Container, Menu, Grid, Icon, Label } from "semantic-ui-react";
 import Link from "next/link";
+import BasicModal from "../../Modal/BasicModal/BasicModal";
 
 export default function MenuWeb() {
+  const [showModal, setShowModal] = useState(false);
+
+  const onShowModal = () => setShowModal(true);
+
   return (
     <div className="menu">
       <Container>
@@ -10,10 +16,18 @@ export default function MenuWeb() {
             <MenuPlatforms />
           </Grid.Column>
           <Grid.Column className="menu__right" width={10}>
-            <MenuOptions />
+            <MenuOptions onShowModal={onShowModal} />
           </Grid.Column>
         </Grid>
       </Container>
+      <BasicModal
+        show={showModal}
+        setShow={setShowModal}
+        title="Inicia sesión"
+        size="small"
+      >
+        <h2>Contenido del modal</h2>
+      </BasicModal>
     </div>
   );
 }
@@ -34,10 +48,10 @@ function MenuPlatforms() {
   );
 }
 
-function MenuOptions() {
+function MenuOptions({ onShowModal }) {
   return (
     <Menu>
-      <Menu.Item>
+      <Menu.Item onClick={onShowModal}>
         <Icon name="user outline" />
         Mi Cuenta
       </Menu.Item>
