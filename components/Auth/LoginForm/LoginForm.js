8dc3d9f7d1420e01de6f42cee96fc00f94f3c790
victormmorales <victor.morales@criptan.com>
2022-03-01 09:01:45 +0100

    login form

--- components/Auth/LoginForm/LoginForm.js
+++ components/Auth/LoginForm/LoginForm.js
@@ -3,18 +3,50 @@ import { Form, Button } from "semantic-ui-react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { toast } from "react-toastify";
+import { loginApi } from "../../../api/user";
 
-export default function LoginForm({ showRegisterForm }) {
+export default function LoginForm({ showRegisterForm, onCloseModal }) {
+  const [loading, setLoading] = useState(false);
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: async (formData) => {
+      setLoading(true);
+      const response = await loginApi(formData);
+      if (response?.jwt) {
+        console.log(response.user.name);
+        toast.success(`¡Hola ${response.user.name}! Nos alegramos de verte 🥳`);
+        onCloseModal();
+      } else {
+        toast.error("El email o la contraseña son incorrectos 😱");
+      }
+      setLoading(false);
+    },
+  });
+
   return (
-    <Form className="login-form">
-      <Form.Input name="identifier" type="text" placeholder="Email" />
-      <Form.Input name="password" type="password" placeholder="Contraseña" />
+    <Form className="login-form" onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="identifier"
+        type="text"
+        placeholder="Email"
+        onChange={formik.handleChange}
+        error={formik.errors.identifier}
+      />
+      <Form.Input
+        name="password"
+        type="password"
+        placeholder="Contraseña"
+        onChange={formik.handleChange}
+        error={formik.errors.password}
+      />
       <div className="actions">
         <Button type="button" basic onClick={showRegisterForm}>
           ¡Registrate!
         </Button>
         <div>
-          <Button className="submit" type="submit">
+          <Button className="submit" type="submit" loading={loading}>
             Entrar
           </Button>
           <Button type="button">¡Has olvidado tu contraseña? 🤔</Button>
@@ -22,4 +54,18 @@ export default function LoginForm({ showRegisterForm }) {
       </div>
     </Form>
   );
+}
+
+function initialValues() {
+  return {
+    identifier: "",
+    password: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    identifier: Yup.string().email(true).required(true),
+    password: Yup.string().required(true),
+  };
 }
